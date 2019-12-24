ad9f027cb2c6b619641f90107c894a8d20f7ed88
Sumuga Padman <sumugapadman@yahoo.in>
2019-12-25 00:23:47 +0530

    LWC OSS

---
+++ src/client/modules/my/app/app.js
@@ -0,0 +1,27 @@
+import { LightningElement, track } from 'lwc';
+
+export default class App extends LightningElement {
+    @track sessionId;
+    @track state;
+    constructor() {
+        super();
+        this.state = 'list';
+        window.history.replaceState('list', null, '');
+        window.onpopstate = event => {
+            if (event.state) {
+            this.state = event.state;
+            }
+        };
+    }
+    get isStateList() {
+        return this.state === 'list';
+    }
+    get isStateDetails() {
+        return this.state === 'details';
+    }
+    handleNavigate(event) {
+        this.sessionId = event.detail;
+        this.state = 'details';
+        window.history.pushState('details', null);
+    }
+}
